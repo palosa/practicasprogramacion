fbebf92d55bfa5ea64acc79a37f202cf8437d8ab
hola <hoola.com>
2016-10-24 20:10:40 +0200

    primer commit

---
+++ multiplicar2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tabla de multiplicar</title>
+  </head>
+  <body>
+<?php
+$a=0;
+while ($a <= 10) {
+  $operacion = $a * 2;
+  echo "2 * $a = $operacion <br>";
+  $a++;
+}
+ ?>
+</html>
