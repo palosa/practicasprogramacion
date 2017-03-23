e1a03f82a4414aa129c0da1e79bdef0bf2ccefb3
palosa <pablodeep14@gmail.com>
2017-03-23 20:19:53 +0100

    Update actualizar.php

--- A22CRUD/actualizar.php
+++ A22CRUD/actualizar.php
@@ -11,19 +11,19 @@
         <form class="" action="actualizarDB.php" method="post">
 
         <p>Nombre:<br>
-        <input type="text" name="nombre" value="<?php $_GET['Nombre'] ?>" readonly="readonly"><br>
+        <input type="text" name="nombre" value="<?= $_GET['Nombre'] ?>" readonly="readonly"><br>
 
         <p>conferencia:<br>
-          <input type="text" name="conferencia" value="<?php $_GET['Ciudad'] ?>"><br>
+          <input type="text" name="conferencia" value="<?= $_GET['Ciudad'] ?>"><br>
 
         <p>Ciudad:<br>
-          <input type="text" name="ciudad" value="<?php $_GET['Conferencia'] ?>"><br>
+          <input type="text" name="ciudad" value="<?= $_GET['Conferencia'] ?>"><br>
 
           <p>Division:<br>
-            <input type="text" name="division" value="<?php $_GET['Division'] ?>"><br><br>
+            <input type="text" name="division" value="<?= $_GET['Division'] ?>"><br><br>
 
 
-        <input type="submit" name="name" value="Actualizar">
+        <input type="submit" value="Actualizar">
 
         </form>
     <?php endif; ?>
